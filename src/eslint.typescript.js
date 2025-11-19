ba96f10d39fe41eb02e055492a8cdc3c98744be7
Huijie Wei <huijiewei@outlook.com>
2025-11-19 16:38:49 +0800

    build: dependencies update

--- src/eslint.typescript.js
+++ src/eslint.typescript.js
@@ -4,30 +4,21 @@ import typescript from "typescript-eslint";
 import javascript from "./eslint.javascript.js";
 
 /** @type {import("eslint").Linter.Config[]} */
-const config = defineConfig(
-  javascript,
-  typescript.configs.recommended,
-  {
-    name: "typescript/erasable-syntax-only",
-    files: ["**/*.ts", "**/*.tsx"],
-    ...erasableSyntaxOnly.configs.recommended,
-  },
-  {
-    name: "resolid/typescript",
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-      },
-    },
-    rules: {
-      "@typescript-eslint/no-import-type-side-effects": "error",
-      "@typescript-eslint/method-signature-style": ["error", "property"],
-      "@typescript-eslint/prefer-function-type": "error",
-      "@typescript-eslint/prefer-for-of": "error",
-      "@typescript-eslint/consistent-type-imports": "error",
+export default defineConfig(javascript, {
+  name: "resolid/typescript",
+  files: ["**/*.ts", "**/*.tsx"],
+  extends: [typescript.configs.recommended, erasableSyntaxOnly.configs.recommended],
+  languageOptions: {
+    parser: typescript.parser,
+    parserOptions: {
+      projectService: true,
     },
   },
-);
-
-export default config;
+  rules: {
+    "@typescript-eslint/no-import-type-side-effects": "error",
+    "@typescript-eslint/method-signature-style": ["error", "property"],
+    "@typescript-eslint/prefer-function-type": "error",
+    "@typescript-eslint/prefer-for-of": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+  },
+});
